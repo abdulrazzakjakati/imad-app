0ec83b728e89556bdf25eb406d379a1d77e92a03
abdulrazzakjakati <30927556+abdulrazzakjakati@users.noreply.github.com>
2017-08-23 14:00:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,80 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var articles = {
+    article-one: {
+	    title: 'Article One | Abdulrazzak Jakati',
+    	heading: 'Article One',
+    	date: 'Sep 5, 2016',
+    	content: `
+    	<p>	
+	    	This is the content of my first article.
+    	</p>`
+    },
+    article-two: {
+    	title:'Article two | Abdulrazzak Jakati',
+    	heading: 'Article two',
+    	date: 'Sep 10, 2016',
+    	content: `
+    	<p>	
+    		This is the content of my second article.
+    	</p>`
+    },
+
+    article-three: {
+    	title:'Article three | Abdulrazzak Jakati',
+    	heading: 'Article three',
+    	date: 'Sep 15, 2016',
+    	content: `
+    	<p>	
+    		This is the content of my third article.
+    	</p>`
+    }
+};
+
+function createTemplate(data) {
+	var title= data.title;
+	var date= data.date;
+	var heading= data.heading;
+	var content= data.content;
+	
+	var htmlTemplate ='
+	<html>
+		<head>
+			<title>
+				${title}
+			</title>
+			<meta name="viewport" content="width=device-width,initial-scale=1" />
+			<link href="/ui/style.css" rel="stylesheet" />
+		</head>
+
+
+		<body>
+			<div class="container">
+				<div>
+					<a href="/">Home</a>
+				</div>
+
+				<h3>
+					${heading}
+				</h3>
+			
+				<div>
+					${date}
+				</div>
+			
+				<div>
+					${content}
+				</div>
+			
+			</div>
+		</body>
+	</html> 
+';
+return htmlTemplate;
+}
+
+
 app.get('/article-one', function (req, res) {
   res.send('Article one will be served');
 });
