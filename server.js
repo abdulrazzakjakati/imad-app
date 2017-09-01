c71f992969df8062315e175a8ce8b821121aa469
abdulrazzakjakati <30927556+abdulrazzakjakati@users.noreply.github.com>
2017-09-01 12:38:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,9 +88,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
+//var counter = 0;
 app.get('/counter', function (req, res) {
-  counter = counter + 1;
+  //counter = counter + 1;
  // res.send(counter.toString());
  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
