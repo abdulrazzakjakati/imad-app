4db91e085c05803f199eceb8c0ec3f23d87b7388
abdulrazzakjakati <30927556+abdulrazzakjakati@users.noreply.github.com>
2017-09-01 16:57:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,9 +84,9 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+//app.get('/', function (req, res) {
+ // res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+//});
 
 var counter = 0;
 app.get('/counter', function (req, res) {
