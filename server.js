a4adeaed5c95db69dec8b42dc60841321aec9835
abdulrazzakjakati <30927556+abdulrazzakjakati@users.noreply.github.com>
2017-09-01 11:38:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/ui/main.js', function (req, res) {
+  counter++;
+  res.send(couner.toString());
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
