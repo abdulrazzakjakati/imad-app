f579a4801c233478bd89d859ff3bb976a66ed859
abdulrazzakjakati <30927556+abdulrazzakjakati@users.noreply.github.com>
2017-09-01 19:56:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,10 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
+app.get('/favicon.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
