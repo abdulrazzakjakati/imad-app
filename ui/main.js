bda16ed3e3b4e78c80407c9f2975d67983336dc9
abdulrazzakjakati <30927556+abdulrazzakjakati@users.noreply.github.com>
2017-09-01 11:30:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 //change the text of inner html
 var element=document.getElementById('main-text');
-element.innerHTML='new value';
+//element.innerHTML='new value';
 
 //Move the Image
 var img = document.getElementById('madi');
