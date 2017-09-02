137c581163b43a6d88f87532ca924a1e39f63b32
abdulrazzakjakati <30927556+abdulrazzakjakati@users.noreply.github.com>
2017-09-02 22:55:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,26 @@ button.onclick = function() {
 	request.open('GET','http://abdulrazzakjakati.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+//Submit name
+var 
+button.onclick = function() {
+    //make a request to counter endpoint
+    var request = new XMLHttpRequest();
+    //Capture the responce & store it in variable.  
+    request.onreadystatechange = function () {
+	    if(request.readyState === XMLHttpRequest.DONE){
+		    // Take some action
+	    	if(request.status === 200){
+		    	var counter = request.responseText;
+		    	var span = document.getElementById('count');
+			    span.innerHTML = counter.toString();
+		    }
+	    }
+	
+    };
+	
+	//Make a request
+	request.open('GET','http://abdulrazzakjakati.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
